b6306bb6dd7585d3a2fef3278454dd11a5beda75
lab_services_student <lab_services_student@labVMH8OX>
2024-04-02 16:59:25 +0200

    Add project files.

---
+++ WebApplication1/Controllers/userController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class userController : Controller
+    {
+        public Table_1 usrtbl = new Table_1();
+
+        [HttpPost]
+        public ActionResult About(Table_1 Users)
+        {
+            var result = usrtbl.insert_User(Users);
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        [HttpGet]
+        public IActionResult About()
+        {
+            return View(usrtbl);
+        }
+    }
+}
---
+++ WebApplication1/Models/Table_1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Configuration;
+using System.Runtime.InteropServices;
+using System.Data.SqlClient;
+using NuGet.Protocol.Plugins;
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+
+namespace WebApplication1.Models
+{
+    public class Table_1 
+    {
+        public static string con_string = "Server=tcp:cloud-dev-db.database.windows.net,1433;Initial Catalog=cloud-dev-db;Persist Security Info=False;User ID=LukePetzer;Password=WhiteIceBeard44@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+
+        public static SqlConnection con = new SqlConnection(con_string);
+
+        public string Name 
+        {
+            get; set; 
+        }
+
+        public string Surname 
+        { 
+            get; set; 
+        }
+
+        public string Email 
+        { 
+            get; set; 
+        }
+
+        public int insert_User(Table_1 n)
+        {
+            try
+            {
+                string sql = "INSERT INTO Table_1 (userName, userSurname, userEmail) VALUES(@Name, @Surname, @Email)";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Name", n.Name);
+                cmd.Parameters.AddWithValue("@Surname", n.Surname);
+                cmd.Parameters.AddWithValue("@Email", n.Email);
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                con.Close();
+                return rowsAffected;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+
+    }
+
+
+}
